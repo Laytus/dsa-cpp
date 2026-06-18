b51cf614859fa5abd9ef174be5fff53f9c07e368
HeberArteagaJ <heberarteagajimenez@gmail.com>
2026-06-17 21:11:11 -0400

    Update insertion sort utilities

--- include/algorithms/insertion_sort.hpp
+++ include/algorithms/insertion_sort.hpp
@@ -4,6 +4,20 @@
 
 namespace dsa::algorithms {
 
-void insertion_sort(std::vector<int>& input);
+template <typename T>
+void insertion_sort(std::vector<T>& input) {
+    for (std::size_t i = 1; i < input.size(); ++i) {
+        const T key = input[i];
+
+        std::size_t j = i;
+
+        while(j > 0 && input[j - 1] > key) {
+            input[j] = input[j - 1];
+            --j;
+        }
+
+        input[j] = key;
+    }
+}
 
 }  // namespace dsa::algorithms
